86226fef1e0ed20c10d5c05f7bae82b2fcf2679a
maskovli <42065097+maskovli@users.noreply.github.com>
2021-07-05 10:23:58 +0200

    Update New-LABClient.ps1

--- Hyper-V (Lab)/New-LABClient.ps1
+++ Hyper-V (Lab)/New-LABClient.ps1
@@ -16,13 +16,10 @@
 Set the Variables needed for the operation
 #>
 
-$LabVMName = "LAB-CLIENT-01"
-$LabVMPath = "C:\VMs\$LabVMName"
+$LabVMName = "CTG-CLIENT-01"
+$LabVMPath = "D:\VMs\$LabVMName"
 $Time = "Get-Date"
-$ISO = "C:\Lab\Image\ISO\Windows\Win10_1909\Win10_Custom.iso"
-$vmDvdDrive = Get-VMDvdDrive -VMName $LabVMName
-$vmNetworkAdapter = get-VMNetworkAdapter -VMName $LabVMName
-$vmHardDiskDrive = get-VMHardDiskDrive -VMName $LabVMName
+$ISO = "D:\iso\en_windows_10_business_editions_version_21h1_updated_jun_2021_x64_dvd_8222df3d.iso"
 
 <#
 Create the VM
@@ -31,15 +28,19 @@ Create the VM
 New-VM -Name $LabVMName -MemoryStartupBytes 4GB -BootDevice VHD -NewVHDPath "$LabVMPath\Virtual Hard Disks\$LabVMName.vhdx" -Path "C:\VMs\$LabVMName\Virtual Machines" -NewVHDSizeBytes 80GB -Generation 2 -Switch "Default switch" -Verbose
 Set-VM -Name $LabVMName -ProcessorCount 4 -AutomaticCheckpointsEnabled $False -SnapshotFileLocation "$LabVMPath\Snapshots" -Verbose
 Add-VMDvdDrive -Path $ISO -VMName $LabVMName -Verbose
+Get-VMNetworkAdapter -VMName $LabVMName | Connect-VMNetworkAdapter -SwitchName Redd0g
 
 <#
 Enable Security features and other settings
-#>
 
 $owner = Get-HgsGuardian UntrustedGuardian
 $kp = New-HgsKeyProtector -Owner $owner -AllowUntrustedRoot
 Set-VMKeyProtector -VMName $LabVMName -KeyProtector $kp.RawData
 Enable-VMTPM -VMName $LabVMName
+
+#>
+Set-VMKeyProtector -VM $LabVMName -NewLocalKeyProtector
+Enable-VMTPM $LabVMName
 
 <#
 Set boot order to DVD Drive
