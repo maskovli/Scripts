223fc119a26d506328ea030f3a6f921a1955a5ed
maskovli <42065097+maskovli@users.noreply.github.com>
2023-04-07 18:33:44 +0200

    Added catch and error handling.

--- Hyper-V (Lab)/New-LABClient.ps1
+++ Hyper-V (Lab)/New-LABClient.ps1
@@ -10,56 +10,53 @@
         TIP: Add the autounattend.xml to the ISO in order to automate the process entierly. 
 #>
 
-
-
-<#
-Set the Variables needed for the operation
-#>
-
+# Define variables
 $LabVMName = "SH-CLIENT-05-PersonalEnv"
 $LabVMPath = "D:\VMs\$LabVMName"
-$Time = "Get-Date"
+$Time = Get-Date
 $ISO = "D:\iso\en-us_windows_11_business_editions_updated_may_2022_x64_dvd_f6700d97.iso"
 $Switch = "vLAN"
 
-<#
-Create the VMs
-#>
+# Create the VM
+try {
+    New-VM -Name $LabVMName -MemoryStartupBytes 4GB -BootDevice VHD -NewVHDPath "$LabVMPath\Virtual Hard Disks\$LabVMName.vhdx" -Path "C:\VMs\$LabVMName\Virtual Machines" -NewVHDSizeBytes 80GB -Generation 2 -Switch "Default switch" -Verbose
+    Set-VM -Name $LabVMName -ProcessorCount 4 -AutomaticCheckpointsEnabled $False -SnapshotFileLocation "$LabVMPath\Snapshots" -Verbose
+    Add-VMDvdDrive -Path $ISO -VMName $LabVMName -Verbose
+    Get-VMNetworkAdapter -VMName $LabVMName | Connect-VMNetworkAdapter -SwitchName $Switch
+} catch {
+    Write-Error "Error creating VM: $_"
+    exit 1
+}
 
-New-VM -Name $LabVMName -MemoryStartupBytes 4GB -BootDevice VHD -NewVHDPath "$LabVMPath\Virtual Hard Disks\$LabVMName.vhdx" -Path "C:\VMs\$LabVMName\Virtual Machines" -NewVHDSizeBytes 80GB -Generation 2 -Switch "Default switch" -Verbose
-Set-VM -Name $LabVMName -ProcessorCount 4 -AutomaticCheckpointsEnabled $False -SnapshotFileLocation "$LabVMPath\Snapshots" -Verbose
-Add-VMDvdDrive -Path $ISO -VMName $LabVMName -Verbose
-Get-VMNetworkAdapter -VMName $LabVMName | Connect-VMNetworkAdapter -SwitchName $Switch
+# Enable security features and other settings
+try {
+    Set-VMKeyProtector -VMName $LabVMName -NewLocalKeyProtector -Verbose
+    Enable-VMTPM -VMName $LabVMName -Verbose
+} catch {
+    Write-Error "Error enabling security features: $_"
+    exit 1
+}
 
-<#
-Enable Security features and other settings
+# Set boot order to DVD drive
+try {
+    $vmFirmware = Get-VMFirmware $LabVMName
+    $bootOrder = $vmFirmware.BootOrder
+    $hddrive = $bootOrder[0]
+    $pxe = $bootOrder[1]
+    $dvddrive = $bootOrder[2]
+    Set-VMFirmware -VMName $LabVMName -BootOrder $dvddrive,$hddrive,$pxe -Verbose
+} catch {
+    Write-Error "Error setting boot order: $_"
+    exit 1
+}
 
-$owner = Get-HgsGuardian UntrustedGuardian
-$kp = New-HgsKeyProtector -Owner $owner -AllowUntrustedRoot
-Set-VMKeyProtector -VMName $LabVMName -KeyProtector $kp.RawData
-Enable-VMTPM -VMName $LabVMName
-
-#>
-Set-VMKeyProtector -VMName $LabVMName -NewLocalKeyProtector -Verbose
-Enable-VMTPM -VMName $LabVMName -Verbose
-
-<#
-Set boot order to DVD Drive
-#>
-
-#Get-VMFirmware $LabVMName
-$win10g2 = Get-VMFirmware $LabVMName
-$win10g2.bootorder
-$hddrive = $win10g2.BootOrder[0]
-$pxe = $win10g2.BootOrder[1]
-$dvddrive = $win10g2.BootOrder[2]
-Set-VMFirmware -VMName $LabVMName -BootOrder $dvddrive,$hddrive,$pxe -Verbose
-
-<#
-Start the VM and connect to the console
-#>
-
-vmconnect.exe localhost $LabVMName
-Start-VM -VMName $LabVMName -Verbose
+# Start the VM and connect to the console
+try {
+    vmconnect.exe localhost $LabVMName
+    Start-VM -VMName $LabVMName -Verbose
+} catch {
+    Write-Error "Error starting VM: $_"
+    exit 1
+}
 
 #Checkpoint-VM -VMName $LabVMName -SnapshotName "$LabVMName-$Time"
